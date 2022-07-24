0c9bece62148d8f5901c4b242becb6bf47990ba0
Christophe Thibaut <cthibaut@octo.com>
2022-07-24 22:18:58 +0200

    the plan

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+
 fn main() {
     println!("Hello, world!");
 }
@@ -35,6 +36,7 @@ mod tests {
     #[test]
     fn given_no_driver_result_is_never() {
         let routes = vec![];
+       // let routes = network![]; // should not compile : network with not routes
         assert_eq!(None, minute_to_complete_gossip(routes))
     }
 
@@ -42,6 +44,8 @@ mod tests {
     fn given_no_common_stops_result_is_never() {
         let routes = vec![vec![42]
                          ,vec![17]];
+      //  let routes = network![line![42]   // should not compile : line with less than 2 stops
+      //                       ,line![17]];
         assert_eq!(None, minute_to_complete_gossip(routes))
     }
     #[test]
@@ -54,6 +58,8 @@ mod tests {
     fn given_two_commons_stop_at_minute_1_result_is_one() {
         let routes = vec![vec![17,42]
                          ,vec![23,42]];
+      //  let routes = network![line![17,42]
+      //                       ,line![23,42]];
         assert_eq!(Some(1), minute_to_complete_gossip(routes))
     }
 }
